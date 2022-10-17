83f336df780ccd48f2202e25a8302b55ff903d12
Mo <55099505+moktermd08@users.noreply.github.com>
2022-10-17 15:03:45 +0100

    Update My-top-100-PHP-functions.php
    functions

--- My-top-100-PHP-functions.php
+++ My-top-100-PHP-functions.php
@@ -1,1 +1,128 @@
 
+## String functions 
+## Array functions 
+## Number functions 
+## Calendar functions 
+## Date functions 
+## Directory functions 
+## Error functions 
+## Exception functions 
+## Filesystem functions 
+## Filter functions 
+## FTP functions 
+## JSON functions 
+## Keywords functions 
+## Libxml functions 
+## Mail functions 
+## Math functions 
+## Misc functions 
+## MySQLi functions 
+## Network functions 
+## Output functions 
+## RegEx functions 
+## SimpleXML functions 
+## Stream functions 
+## Variable Handling functions  
+## XML Parser functions 
+## Zip Timezones functions 
+
+
+is_array (Check array or not) 
+substr ( Take part of a string ) 
+in_array (check if something is in array ) 
+explode ( Convert string into array ) 
+str_replace ( replace string ) 
+implode ( convert array into string 
+strlen ( Count lenth of string ) 
+array_merge ( Combine two array into one, second array keys value takes priority if similar value exist ) 
+strpos (search two string and find position of first occurance unless offset have any value)
+preg_match ( Perform a regular expression search and match )
+sprintf ( formate a string into given formate )
+trim ( Trim white space from string ) 
+strtolower (make string lower case ) 
+file_exists ( check if a file exist ) 
+is_string ( check if something is stirng or not ) 
+preg_replace  ( use to search string and return string or array based on match ) 
+file_get_contents (read file into array  ) 
+array_key_exists ( check array key ) 
+array_keys (return keys of any given array)
+dirname ( return parth of the parent directory ) 
+function_exists ( check if a functio nexist ) 
+array_map (to apply call back to a function elements by another)
+get_class ( return the name of class from an object ) 
+class_exists ( class exist or not checker ) 
+is_object ( check if obejct or not ) 
+time (return unix time stamp)
+json_encode (convert a value into json formate)
+date (formate timestamp into string )
+is_null ( check if value is null ) 
+is_numeric ( check if value is numeric ) 
+array_shift (remove first element of an array ) 
+defined ( used to define a constant ) 
+is_dir ( check if a file or directory ) 
+json_decode ( reverse the json data to compatable for php ) 
+header ( used to send raw http header ) 
+strtoupper ( convert string to upper case ) 
+array_values (print all the arrya values )
+md5 (Calculate hash string of md5 )  
+method_exists ( used to check if a method exsist inside a class ) 
+file_put_contents ( used to put data into file ) 
+rtrim ( remove white space from the endo f the string ) 
+array_pop ( oppose to shift it remove the last element of an arrya ) 
+unlink ( remove a value from ) 
+basename ( get path of the current file as string ) 
+realpath ( provide canonical absulate path ) 
+call_user_func ( Call the callback given by the first parameter ) 
+call_user_func_array (Call the callback given by the first parameter as aray ) 
+fopen ( opens a file or url ) 
+microtime (unix time stamp with microseconds)
+fclose ( to close an open file or url ) 
+is_int ( 
+is_file
+array_slice
+preg_match_all
+ucfirst
+intval
+str_repeat
+serialize
+array_filter
+mkdir
+is_callable
+ltrim
+ob_start
+round
+fwrite
+array_unique
+array_search
+reset
+array_unshift
+parse_url
+func_get_args
+end
+base64_encode
+unserialize
+max
+preg_split
+gettype
+strrpos
+version_compare
+array_push
+floor
+strtotime
+htmlspecialchars
+ini_get
+ini_set
+chr
+extension_loaded
+is_bool
+ksort
+array_reverse
+ord
+uniqid
+strtr
+array_diff
+error_reporting
+ceil
+urlencode
+min
+print_r
