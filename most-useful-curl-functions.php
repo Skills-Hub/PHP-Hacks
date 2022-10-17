dd8827d5e32573bca1de88b30d271cdf535f7f73
Mo <55099505+moktermd08@users.noreply.github.com>
2022-10-17 14:59:41 +0100

    Create most-useful-curl-functions.php
    curl request that require basic authintication

---
+++ most-useful-curl-functions.php
@@ -0,0 +1,27 @@
+$username='';
+$password='';
+$URL='';
+
+function curlRequest($username, $password, $url) { 
+$username = $username;
+$password = $password;;
+$url = $url;
+
+$ch = curl_init();
+$headr = array();
+$headr[] = 'Content-length: 0';
+$headr[] = 'Content-type: application/json';
+$headr[] = 'Authorization: Basic '. base64_encode($username.':'.$password);
+curl_setopt($ch, CURLOPT_HTTPHEADER,$headr);
+curl_setopt($ch, CURLOPT_URL,$URL);
+curl_setopt($ch, CURLOPT_TIMEOUT, 30); //timeout after 30 seconds
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
+$projects= curl_exec ($ch);
+$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);   //get status code
+curl_close ($ch);
+
+return $projects; 
+
+}
+
